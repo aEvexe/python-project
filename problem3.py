5b4d36eb4be5df18bcf888d8d49bbe586bbea43a
aCooper <beknazarovjalol888@gmail.com>
2025-02-14 16:30:11 +0500

    change

--- problem3.py
+++ problem3.py
@@ -1,13 +1,6 @@
 def max_movies_to_watch(movies: list) -> tuple:
     movies.sort(key=lambda x: x[1])
 
-    new_movies = []
-    last_ended_time = -1
-    for i, j in movies:
-        if i >= last_ended_time:
-            new_movies.append((i, j))
-            last_ended_time = j
-
     return len(new_movies), new_movies
 
 
